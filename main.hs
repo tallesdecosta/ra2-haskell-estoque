99bf910b5f95570a3c58d660621c8bf0d23ec6d0
talles telles. <107369815+tallesdecosta@users.noreply.github.com>
2025-11-14 22:48:09 -0300

    Update main.hs
    fui fazendo no gbd compiler

--- main.hs
+++ main.hs
@@ -1,135 +1,111 @@
+-- Módulos importados
 import qualified Data.Map.Strict as Map
 import Data.Time (UTCTime, getCurrentTime)
 import Control.Exception (catch, SomeException, fromException, evaluate)
 import System.IO (IOMode(..), openFile, hClose, hGetContents, readFile, writeFile, appendFile)
 import System.IO.Error (isDoesNotExistError)
-import Data.List (sortBy, groupBy, sortOn, isInfixOf)
+import Data.List (sortBy, groupBy, sortOn, isInfixOf, elemIndex)
 import Data.Ord (Down(..), comparing)
 import Data.Maybe (mapMaybe)
 
 
 data Item = Item {
-
     itemID :: String,
-    
     nome :: String,
-    
     quantidade :: Int,
-    
     categoria :: String
-    
 } deriving (Show, Read)
-
-
 type Inventario = Map.Map String Item
-
 
 data AcaoLog = Add | Remove | UpdateQty | QueryFail | Report
     deriving (Show, Read, Eq)
 
-
 data StatusLog = Sucesso | Falha String
     deriving (Show, Read, Eq)
 
-
 data LogEntry = LogEntry {
-
-    timestamp :: UTCTime,
-    
+    timestamp :: UTCTime, 
     acao :: AcaoLog,
-    
     detalhes :: String,
-    
     status :: StatusLog
-    
 } deriving (Show, Read)
 
 
 type ResultadoOperacao = (Inventario, LogEntry)
 
 
+
+
 addItem :: UTCTime -> String -> String -> Int -> String -> Inventario -> Either String ResultadoOperacao
 addItem agora id nome qtde cat inv =
-
     if Map.member id inv
-    
-        then Left "❌ERRO AO ADICIONAR UM ITEM: Já existe um item com esse id no inventário. :("
-        
+
+        then Left "ERRO AO ADICIONAR UM ITEM: Já existe um item com esse id no inventário. :("
+
         else
-
             let novoItem = Item { itemID = id, nome = nome, quantidade = qtde, categoria = cat }
-
                 novoInv = Map.insert id novoItem inv
-
                 detalhesOp = "ID =" ++ id ++ " Nome =" ++ nome ++ " Quantidade =" ++ show qtde ++ " Categoria =" ++ cat
                 
+
                 logEntry = LogEntry {
-                
                     timestamp = agora,
                     acao = Add,
                     detalhes = detalhesOp,
                     status = Sucesso
-                    
                 }
+            in Right (novoInv, logEntry) 
 
-            in Right (novoInv, logEntry)
-
-
-atualizarEstoque :: UTCTime -> String -> Int -> Inventario -> Either String ResultadoOperacao
-atualizarEstoque agora id qtdeMudar inv =
-
+updateQty :: UTCTime -> String -> Int -> Inventario -> Either String ResultadoOperacao
+updateQty agora id qtdeMudar inv =
     case Map.lookup id inv of
-    
-        Nothing -> Left "❌ERRO AO ATUALIZAR ESTOQUE: Não existe item com esse ID no inventário. :("
+
+        Nothing -> Left "ERRO AO ATUALIZAR ESTOQUE: Não existe item com esse ID no inventário. :("
         
+
         Just itemEncontrado ->
-        
             let novaQtde = quantidade itemEncontrado + qtdeMudar
             
+
             in if novaQtde < 0
-            
-                then Left "❌ERRO AO ATUALIZAR ESTOQUE: Não há estoque suficiente para realizar essa movimentação. :(" 
+                then Left "ERRO AO ATUALIZAR ESTOQUE: Não há estoque suficiente para realizar essa movimentação. :("  
                 
+
                 else
-                
                     let itemAtualizado = itemEncontrado { quantidade = novaQtde }
-                    
                         novoInv = Map.insert id itemAtualizado inv
                         
                         detalhesOp = "ID = " ++ id ++ " Quantidade movimentada = " ++ show qtdeMudar ++ " Novo total = " ++ show novaQtde
                         
                         logEntry = LogEntry {
-                        
                             timestamp = agora,
-                            
-                            acao = if qtdeMudar > 0 then UpdateQty else Remove,
-                            
+                            -- Se a quantidade for positiva, é UpdateQty, senão é Remove.
+                            acao = if qtdeMudar > 0 then UpdateQty else Remove, 
                             detalhes = detalhesOp,
-                            
                             status = Sucesso
-                            
                         }
                         
                     in Right (novoInv, logEntry)
 
 
-removeItem :: UTCTime -> String -> Int -> Inventario -> Either String ResultadoOperacao
-removeItem agora id qtde inv =
+removeltem :: UTCTime -> String -> Int -> Inventario -> Either String ResultadoOperacao
+removeltem agora id qtde inv =
+    updateQty agora id (-qtde) inv -- Reutiliza a lógica de updateQty
 
-    atualizarEstoque agora id (-qtde) inv
 
 
 logsDeErro :: [LogEntry] -> [LogEntry]
 logsDeErro logs = filter eDeErro logs
     where
-      eDeErro (LogEntry _ _ _ (Falha _)) = True
+      eDeErro (LogEntry _ _ _ (Falha _)) = True 
       eDeErro _ = False 
-      
 
-historicoPorItem :: String -> [LogEntry] -> [LogEntry]
-historicoPorItem id logs = filter (eDoItem id) logs
+historicoPorltem :: String -> [LogEntry] -> [LogEntry]
+historicoPorltem id logs = filter (eDoItem id) logs
     where
+
       eDoItem id (LogEntry _ _ det _) = ("ID =" ++ id) `isInfixOf` det
+
 
 
 itemMaisMovimentado :: [LogEntry] -> Maybe (String, Int)
@@ -144,51 +120,42 @@ itemMaisMovimentado logs =
         contagensOrdenadas = sortOn (Down . snd) contagens
 
     in if null contagensOrdenadas
+       then Nothing
+       else Just (head contagensOrdenadas)
     
-        then Nothing
-        
-        else Just (head contagensOrdenadas)
-        
     where
 
-        extrairID :: LogEntry -> Maybe String
-        extrairID (LogEntry _ _ detalhes (Falha _)) = Nothing 
-        extrairID (LogEntry _ QueryFail _ _) = Nothing       
-        extrairID (LogEntry _ Report _ _) = Nothing         
-        extrairID (LogEntry _ _ detalhes Sucesso) =
-            let partes = words (map (\c -> if c == '=' then ' ' else c) detalhes)
-            in case "ID" `elemIndex` partes of
-                Just i -> if (i+1) < length partes then Just (partes !! (i+1)) else Nothing
-                Nothing -> Nothing
-        
-
-        elemIndex :: Eq a => a -> [a] -> Maybe Int
-        elemIndex _ [] = Nothing
-        elemIndex x (y:ys)
-            | x == y    = Just 0
-            | otherwise = (1 +) <$> elemIndex x ys
+      extrairID :: LogEntry -> Maybe String
+      extrairID (LogEntry _ _ _ (Falha _)) = Nothing 
+      extrairID (LogEntry _ QueryFail _ _) = Nothing 
+      extrairID (LogEntry _ Report _ _) = Nothing 
+      extrairID (LogEntry _ _ detalhes Sucesso) = 
+          let partes = words (map (\c -> if c == '=' then ' ' else c) detalhes)
+          in case "ID" `elemIndex` partes of
+              Just i -> if (i+1) < length partes then Just (partes !! (i+1)) else Nothing
+              Nothing -> Nothing
 
 
 inventarioFile = "Inventario.dat"
 logFile = "Auditoria.log"
 
+
 safeReadFile :: FilePath -> a -> (String -> a) -> IO a
 safeReadFile path defaultVal parser =
     catch (do
+
         contents <- readFile path
+
         let parsed = parser contents
-        
-        evaluate parsed
-        
+        evaluate parsed 
     )
 
     (\(e :: SomeException) -> do
 
         case fromException e :: Maybe IOError of
-
             Just ioerr | isDoesNotExistError ioerr -> do
                 putStrLn $ "Aviso: Arquivo '" ++ path ++ "' nao encontrado. Iniciando com dados padrao."
-                return defaultVal
+                return defaultVal 
 
             _ -> do
                 putStrLn $ "Aviso: Arquivo '" ++ path ++ "' corrompido ou ilegivel. Iniciando com dados padrao."
@@ -196,42 +163,43 @@ safeReadFile path defaultVal parser =
     )
     
 
+
 loadState :: IO (Inventario, [LogEntry])
 loadState = do
     putStrLn $ "Carregando " ++ inventarioFile ++ "..."
-    
+
     inv <- safeReadFile inventarioFile Map.empty read
     putStrLn $ "Carregando " ++ logFile ++ "..."
-    
-    logs <- safeReadFile logFile [] (map read . lines)
+
+    logs <- safeReadFile logFile [] (map read . lines) 
     putStrLn "Estado carregado."
     return (inv, logs)
 
 
 persistSucesso :: Inventario -> LogEntry -> IO ()
 persistSucesso inv logEntry = do
-    writeFile inventarioFile (show inv)
-    appendFile logFile (show logEntry ++ "\n")
 
+    writeFile inventarioFile (show inv)
+
+    appendFile logFile (show logEntry ++ "\n")
 
 persistFalha :: LogEntry -> IO ()
 persistFalha logEntry = do
 
     appendFile logFile (show logEntry ++ "\n")
 
-
 main :: IO ()
 main = do
     putStrLn "--- Sistema de Gerenciamento de Inventario (Haskell) ---"
-    
     (inv, logs) <- loadState
-    
     gameLoop inv logs
     putStrLn "--- Encerrando. ---"
 
 
+
 gameLoop :: Inventario -> [LogEntry] -> IO ()
 gameLoop inventario logs = do
+
     putStrLn "\nComandos:"
     putStrLn "  add <id> <nome> <qtde> <categoria>"
     putStrLn "  estoque <id> <qtde_mudar> (ex: 10 para adicionar, -5 para remover)"
@@ -243,52 +211,68 @@ gameLoop inventario logs = do
     putStrLn "  sair"
     putStr "Seu comando > "
 
+
     linha <- getLine
     let comando = words linha
-    agora <- getCurrentTime
+    agora <- getCurrentTime 
+
 
     case comando of
+
         ["add", id, nome, qtdeStr, cat] -> do
             let qtde = read qtdeStr :: Int
+
             let resultado = addItem agora id nome qtde cat inventario
+
+            processarResultado agora Add comando resultado
             
-            processarResultado agora comando resultado
-            
+
         ["estoque", id, qtdeStr] -> do
             let qtde = read qtdeStr :: Int
-            let resultado = atualizarEstoque agora id qtde inventario
-            
-            processarResultado agora comando resultado
+
+            let resultado = updateQty agora id qtde inventario 
+            let acaoTentada = if qtde > 0 then UpdateQty else Remove
+            processarResultado agora acaoTentada comando resultado
 
         ["remover", id, qtdeStr] -> do
             let qtde = read qtdeStr :: Int
-            let resultado = removeItem agora id qtde inventario
-            
-            processarResultado agora comando resultado
+
+            let resultado = removeltem agora id qtde inventario 
+            processarResultado agora Remove comando resultado
+
 
         ["listar"] -> do
             putStrLn "--- Inventario Atual ---"
             mapM_ print (Map.elems inventario)
-            gameLoop inventario logs
+            gameLoop inventario logs 
             
+
         ["relatorio", "erros"] -> do
             putStrLn "--- Relatorio de Erros ---"
-            mapM_ print (logsDeErro logs)
-            gameLoop inventario logs
+
+            mapM_ print (logsDeErro logs) 
+            gameLoop inventario logs 
+
 
         ["relatorio", "item", id] -> do
             putStrLn $ "--- Historico do Item " ++ id ++ " ---"
-            mapM_ print (historicoPorItem id logs)
+
+            mapM_ print (historicoPorltem id logs)
             gameLoop inventario logs
+
 
         ["relatorio", "mais_movimentado"] -> do
             putStrLn $ "--- Item Mais Movimentado (Sucesso) ---"
-            print (itemMaisMovimentado logs)
-            gameLoop inventario logs
+
+            print (itemMaisMovimentado logs) 
+            gameLoop inventario logs 
+
 
         ["sair"] -> do
             putStrLn "Salvando e saindo..."
+
             
+
         _ -> do
             putStrLn "Comando invalido ou numero incorreto de argumentos."
             let logFalha = LogEntry {
@@ -297,26 +281,33 @@ gameLoop inventario logs = do
                 detalhes = unwords comando, 
                 status = Falha "Comando invalido"
             }
+
             persistFalha logFalha
             gameLoop inventario (logs ++ [logFalha])
 
+
     where
+        processarResultado :: UTCTime -> AcaoLog -> [String] -> Either String ResultadoOperacao -> IO ()
+        
 
-      processarResultado :: UTCTime -> [String] -> Either String ResultadoOperacao -> IO ()
-      
+        processarResultado agora acaoTentada comando (Left erroMsg) = do
+            putStrLn $ "FALHA: " ++ erroMsg
 
-      processarResultado agora comando (Left erroMsg) = do
-          putStrLn $ "FALHA: " ++ erroMsg
-          let logFalha = LogEntry {
+            let logFalha = LogEntry {
                 timestamp = agora, 
-                acao = QueryFail, 
+                acao = acaoTentada, 
                 detalhes = unwords comando, 
                 status = Falha erroMsg
-          }
-          persistFalha logFalha
-          gameLoop inventario (logs ++ [logFalha])
+            }
 
-      processarResultado _ _ (Right (novoInv, logSucesso)) = do
-          putStrLn "SUCESSO: Operacao registrada."
-          persistSucesso novoInv logSucesso
-          gameLoop novoInv (logs ++ [logSucesso])
+            persistFalha logFalha
+
+            gameLoop inventario (logs ++ [logFalha])
+
+
+        processarResultado _ _ _ (Right (novoInv, logSucesso)) = do
+            putStrLn "SUCESSO: Operacao registrada."
+
+            persistSucesso novoInv logSucesso
+
+            gameLoop novoInv (logs ++ [logSucesso])
